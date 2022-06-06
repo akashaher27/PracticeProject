95549143ede19295f56cca9238aab7b5749bd541
akash <akash.aher@mediassist.in>
2022-06-06 18:39:13 +0530

    1.Added LeakCanary

--- app/src/main/java/com/example/practiceproject/app/ProjectApplication.kt
+++ app/src/main/java/com/example/practiceproject/app/ProjectApplication.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import com.example.practiceproject.remote.retrofit.ACCESS_TOKEN
 import dagger.hilt.android.HiltAndroidApp
 import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import leakcanary.LeakCanary
 import javax.inject.Inject
 
 
