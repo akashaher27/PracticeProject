4679c7e36d218f7614184c34598f4e677b6e3809
Akash Rajesh Aher <akash.aher@grabtaxi.com>
2022-11-26 17:15:31 +0530

    Reset git Amend

--- app/src/main/java/com/example/practiceproject/view/git/Git.kt
+++ app/src/main/java/com/example/practiceproject/view/git/Git.kt
@@ -10,17 +10,6 @@ class Git() : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate: git test 1")
-        Log.d(TAG, "onCreate: git test 2")
-        Log.d(TAG, "onCreate: git test 3")
-        Log.d(TAG, "onCreate: git test 4")
-        Log.d(TAG, "onCreate: git test for amend-1")
-        Log.d(TAG, "onCreate: git test for amend-2")
-        Log.d(TAG, "onCreate: git test for amend-3")
-        Log.d(TAG, "onCreate: git test for amend-3")
-        Log.d(TAG, "onCreate: git test for amend-5")
-        Log.d(TAG, "onCreate: git test for amend-6")
-        Log.d(TAG, "onCreate: git test for amend-7")
-        Log.d(TAG, "onCreate: git test for amend-8")
+        Log.d(TAG, "onCreate: Reset git Amend")
     }
 }
